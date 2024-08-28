e74a7469f63c71ad7c0919adafca1b22b70a1713
Arpit078 <verma.arpit078@gmail.com>
2024-08-28 23:46:05 +0530

    finally

--- api/lib/redis_wrapper.js
+++
@@ -1,9 +0,0 @@
-import { createClient } from 'redis';
-
-const redis_client = createClient();
-
-redis_client.on('error', err => console.log('Redis Client Error', err));
-
-await redis_client.connect();
-
-export { redis_client };
--- db.sql
+++
@@ -1,7 +0,0 @@
--- Creating song hash table :
-    CREATE TABLE songs (
-        track_id VARCHAR(100) PRIMARY KEY,
-        track_name VARCHAR(255),
-        track_artist VARCHAR(255),
-        track_cover VARCHAR(255)
-    );
